90970fbd7d4af269195e149b6b54cfb69b4fc9dd
HermitHseih <90133709+HermitHseih@users.noreply.github.com>
2021-09-05 18:18:23 +0800

    Create sum.py

---
+++ sum.py
@@ -0,0 +1,16 @@
+sumlist=[]
+for i in range(1,11):
+    sumlist.append(i)
+print(sum(sumlist))
+    
+# sum = 0
+# i = 1
+# while i <= 10:
+#     sum = sum + i
+#     i = i + 1
+# print(sum, end= " ")
+
+# sum = 0
+# for i in range(1,11):
+#     sum += i
+# print(sum, end= " ")
